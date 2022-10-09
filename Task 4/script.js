7694ca8806989f20d72d9a91bd953c48d1822c7e
unknown <arnoldasgaigalas@gmail.com>
2022-10-09 16:16:02 +0300

    task 4 Done

--- Task 4/script.js
+++ Task 4/script.js
@@ -10,3 +10,50 @@ bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+
+start(ENDPOINT)
+
+
+function start(carsData) {
+    fetch(carsData)
+    .then (res => res.json())
+    .then (data => {
+        console.log(data);
+        for (i=0; i<data.length; i++){
+            create(data[i])
+        }
+    })
+}
+
+function create(data) {
+    let output = document.getElementById('output')
+
+    output.style.width = '100%'
+
+    let card = document.createElement('div')
+
+    card.style.maxWidth = '80%'
+    card.style.margin = '10px auto'
+    card.style.border = '1px solid'
+    card.style.borderRadius = '5px'
+    card.style.backgroundColor = 'antiquewhite'
+    card.style.padding = '5px'
+
+    let brand = document.createElement('div')
+
+    brand.style.fontSize = '30px'
+
+    brand.innerText = 'Brand : ' + data.brand + "\n Models:"
+
+    card.appendChild(brand)
+    for (b=0; b < data.models.length;b++){
+        let model = document.createElement('div')
+
+        model.innerText = data.models[b]
+        model.style.fontSize = '20px'
+        
+        card.appendChild(model)
+    }
+
+    output.appendChild(card)
+}
