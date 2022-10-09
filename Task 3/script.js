3564b447654144f86f2489203932434cbe9b65db
unknown <arnoldasgaigalas@gmail.com>
2022-10-09 15:32:57 +0300

    task 3 Done

--- Task 3/script.js
+++ Task 3/script.js
@@ -11,3 +11,43 @@ Pastaba: Informacija apie user'į (jo kortelė) bei turi turėti bent minimalų 
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'https://api.github.com/users';
+
+
+document.getElementById('btn').addEventListener('click',showUsers)
+
+function showUsers() {
+    start(ENDPOINT)
+}
+
+function start(FetchData) {
+    document.getElementById('message').innerHTML = ''
+    fetch(FetchData)
+    .then (res => res.json())
+    .then (data => {
+        for (i=0; i < data.length;i++){
+            add(data[i])
+        }
+    })
+}
+
+function add (data) {
+    let output = document.getElementById('output')
+
+    let card = document.createElement('div')
+
+    card.style.display = 'flex'
+    card.style.gap = '10px'
+    card.style.margin = '10px 0px'
+
+    let login = document.createElement('div')
+
+    login.innerText = data.login
+
+    let url = document.createElement('div')
+
+    url.innerText = data.avatar_url
+
+    card.appendChild(login)
+    card.appendChild(url)
+    output.appendChild(card)
+}
