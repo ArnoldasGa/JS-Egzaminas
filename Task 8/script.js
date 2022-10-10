eb8584bdca724f916d60c45fd48fc451ce5f6bde
unknown <arnoldasgaigalas@gmail.com>
2022-10-10 12:55:34 +0300

    task 8 Done

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,27 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą.
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+
+Calculator ()
+
+function Calculator(a,b) {
+    number1 = Number(prompt('Iveskite pirma skaiciu'));
+    number2 = Number(prompt('Iveskite antra skaiciu'));
+    sum = function(){
+        return this.number1 + this.number2
+    }
+    subtraction = function(){
+        return this.number1 - this.number2
+    }
+    multiplication = function(){
+        return this.number1 * this.number2
+    }
+    division = function(){
+        return this.number1 / this.number2
+    }
+    alert(number1 + '+' + number2 + '=' + sum() + '\n' 
+    + number1 + '-' + number2 + '=' + subtraction() + '\n' 
+    + number1 + '*' + number2 + '=' + multiplication() + '\n' 
+    + number1 + '/' + number2 + '=' + division())
+}
